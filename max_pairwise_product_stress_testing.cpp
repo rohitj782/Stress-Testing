221d4fb72a73f68ec64b10d053c206d0510deb10
rohitj782 <rohitj782@gmail.com>
2019-08-07 23:17:35 +0530

    Implementing Stress testing

--- max_pairwise_product_stress_testing.cpp
+++ max_pairwise_product_stress_testing.cpp
@@ -1,78 +1,78 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stdlib.h>
 
-long long MaxPairwiseProduct(const std::vector<int>& numbers) {
+using namespace std;
+
+long long MaxPairwiseProduct(vector<int> numbers) {
     long long max_product = 0;
     int n = numbers.size();
 
-    for (int first = 0; first < n; ++first) {
+    for (int first = 0; first < n; first++) {
         for (int second = first + 1; second < n; ++second) {
-            max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+            max_product = std::max(max_product, ((long long)numbers[first] * numbers[second]));
         }
     }
 
     return max_product;
 }
 
-
-long long  MaxPairwiseProductFast(const std::vector<int>& numbers) {
+long long  MaxPairwiseProductFast(const vector<int>& numbers) {
     long long max_product = 0;
     int n = numbers.size();
-	int max = 0,max2 = 0; 
-	long long maxn1,maxn2;
+	int max1 = 0; 
+	long long maxn1 = 0, maxn2 = 0;
 	for(int i = 0; i < n; i++){
-		if(numbers[i]>max)
-			max = i;
-	}
-	maxn1 = numbers[max];
-
-	for(int i = 0; i < n; i++){
-		if(numbers[i]>max2 && i != max)
-			max2 = i;
+		if(numbers[i]>maxn1){
+			max1 = i;
+			maxn1 = numbers[i];
+		}
 	}
 
-	maxn2 = numbers[max2];
+	for(int i = 0; i < n; i++){
+		if(numbers[i] > maxn2 && i != max1)
+			maxn2 = numbers[i];
+	}
+
 	max_product = ((long long)maxn1) * maxn2;
-
 
     return max_product;
 }
 
 int main() {
 
-while(true){
+	while(true){
+	//generating random input size
+	int inputSize = rand() % 10 +2;
+	cout << inputSize << "\n";
 
-int nn = rand() % 5 +2;
-std::cout << nn;
+    	vector<int> numbers(inputSize);
+    	for (int i = 0; i < inputSize; i++) {
+        	numbers[i] = rand() % 10000+ 1;
+    	}
 
-    std::vector<int> numbers(nn);
-    for (int i = 0; i < nn; ++i) {
-        numbers[i] = rand()%1000;
-    }
+	for (int i = 0; i < inputSize; i++) {
+	        cout << numbers[i] << " ";
+	}
 
-
-    for (int i = 0; i < nn; ++i) {
-        std::cout << numbers[i]+"\n";
-    }
 	long long res1 = MaxPairwiseProduct(numbers);
 	long long res2 = MaxPairwiseProductFast(numbers);
-	if(res1!= res2){
-	std::cout << res1 + "  " + res2;
+	if(res1 != res2){
+	cout << "\n" << res1 << "  " << res2 << "\nFailed";
 	break;	
-   	}else std::cout << "OK\n";
-
+   	} else cout << "\nOK\n";
 }
 
+/*
     int n;
     std::cin >> n;
     std::vector<int> numbers(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> numbers[i];
     }
-
-
     std::cout << MaxPairwiseProduct(numbers) << "\n";
     return 0;
+
+*/
 }
