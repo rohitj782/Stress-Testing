802b76d9049fc212709498bfecb0d58edae7610b
rohitj782 <rohitj782@gmail.com>
2019-08-07 23:04:06 +0530

    Implementing optimal approach

--- max_pairwise_product_fast.cpp
+++ max_pairwise_product_fast.cpp
@@ -7,20 +7,21 @@ using namespace std;
 long long  MaxPairwiseProductFast(const vector<int>& numbers) {
     long long max_product = 0;
     int n = numbers.size();
-	int max1 = 0, max2 = 0; 
-	long long maxn1, maxn2;
+	int max1 = 0; 
+	long long maxn1 = 0, maxn2 = 0;
 	for(int i = 0; i < n; i++){
-		if(numbers[i]>max1)
+		if(numbers[i]>maxn1){
 			max1 = i;
+			maxn1 = numbers[i];
+		}
 	}
-	maxn1 = numbers[max1];
 
 	for(int i = 0; i < n; i++){
-		if(numbers[i] > max2 && i != max1)
-			max2 = i;
+		if(numbers[i] > maxn2 && i != max1)
+			maxn2 = numbers[i];
+
 	}
 
-	maxn2 = numbers[max2];
 	max_product = ((long long)maxn1) * maxn2;
 
 
